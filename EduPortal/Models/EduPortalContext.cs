80f8f36550cfb3406a4174014a58bc6ef603527e
Ema404 <emanhana459@gmail.com>
2022-06-04 19:51:41 +0300

    commit

--- EduPortal/Models/EduPortalContext.cs
+++ EduPortal/Models/EduPortalContext.cs
@@ -54,7 +54,7 @@ namespace EduPortal.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP\\SQLEXPRESS;Database=EduPortal;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-UIL4A1A\\SQLEXPRESS;Database=EduPortal;Trusted_Connection=True;");
             }
         }
 
